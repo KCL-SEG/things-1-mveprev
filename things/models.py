d1f379f867c5d83b4ab37973ea00f9a3b6680bfc
Maksim Veprev <maxveprevspb@gmail.com>
2022-10-25 16:47:18 +0100

    Enforce validation on Thing model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,12 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Thing(models.Model):
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, blank=True)
+    quantity = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
